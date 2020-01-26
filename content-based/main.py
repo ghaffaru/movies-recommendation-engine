2aa3dfe852f1bd7559fd8a48388f941f3f954f31
Ghaffaru <mudashiruagm@gmail.com>
2020-01-26 17:02:15 +0000

    one hot encoding of genres

--- content-based/main.py
+++ content-based/main.py
@@ -18,4 +18,23 @@ movies_df['title'] = movies_df.title.str.replace('(\(\d\d\d\d\))', '')
 
 movies_df['title'] = movies_df['title'].apply(lambda x: x.strip())
 
-print(movies_df.head())
+# print(movies_df.head())
+
+# Every genre is separated by a | so we simply have to call the split function on |
+
+movies_df['genres'] = movies_df.genres.str.split('|')
+
+# print(movies_df.head())
+
+moviesWithGenre_df = movies_df.copy()
+
+# one hot encoding of genres
+
+for index, row in movies_df.iterrows():
+    for genre in row['genres']:
+        moviesWithGenre_df.at[index, genre] = 1
+
+# Filling in the NaN values with 0 to show that a movie doesn't have that column's genre
+
+moviesWithGenres_df = moviesWithGenre_df.fillna(0)
+print(moviesWithGenres_df.head())
